b217d0953b23b55af742012221fea1e103e358f9
satanfps <satanfps.co@gmail.com>
2022-04-25 04:31:24 +0430

    Change of theme btn architecture

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,73 +1,154 @@
 console.clear()
-let $ = document,
+const $ = document,
     _id = $.getElementById,
-    dark =$.getElementsByClassName("dark"),
-    white =$.getElementsByClassName("white"),
-    seprator =$.getElementsByClassName("seprator"),
     _tg = $.getElementsByTagName;
-    
 
-    function whiteMode() {
-        $.getElementById("line").setAttribute("src","images/line.png")
-        $.getElementById("line2").setAttribute("src","images/line.png")
-        $.getElementById("circle").setAttribute("src","images/circle.png")
+let themeFlag= false; //dark theme is off (by default)
 
-        let dark =$.getElementsByClassName("dark");
-        // console.log(dark)
-        for (let index = 0; index < dark.length; index+=1) {
-            if (dark[index].classList.contains("dark")) {
-                dark[index].classList.replace("dark","white");  
-            } 
-        }
 
-        let indentdark =$.getElementsByClassName("indentdark");
-        for (let index = 0; index < indentdark.length; index+=1) {
-            if (indentdark[index].classList.contains("indentdark")) {
-                indentdark[index].classList.replace("indentdark","indentWhite");  
-            } 
-        }
-
-        for (let index = 0; index < seprator.length; index+=1) {
-            seprator[index].setAttribute("src","images/seprator.png");  
-        }}
-
-    function darkMode() {
-        $.getElementById("line").setAttribute("src","images/blackline.png")
-        $.getElementById("line2").setAttribute("src","images/blackline.png")
-        $.getElementById("circle").setAttribute("src","images/blackcircle.png")
-
-        let white =$.getElementsByClassName("white");
-        // console.log(white)
-        for (let index = 0; index < white.length; index+=1) {
-            if (white[index].classList.contains("white")) {
-                white[index].classList.replace("white","dark");  
-            } 
-        }
-
-        let indentWhite =$.getElementsByClassName("indentWhite");
-        for (let index = 0; index < indentWhite.length; index+=1) {
-            if (indentWhite[index].classList.contains("indentWhite")) {
-                indentWhite[index].classList.replace("indentWhite","indentdark");  
-            } 
-        }
-        for (let index = 0; index < seprator.length; index+=1) {
-            seprator[index].setAttribute("src","images/blackseprator.png");  
+const   lineImg  =$.getElementById("line"),
+        lineImg2 =$.getElementById("line2"),
+        circleImg=$.getElementById("circle"),
+        logo     =$.getElementsByClassName("logo")[0]    
+    function picsHandler() {
+        if (!themeFlag) {
+            logo.classList.add("nobrightness")
+            lineImg.setAttribute("src","images/blackline.png")
+            lineImg2.setAttribute("src","images/blackline.png")
+            circleImg.setAttribute("src","images/blackcircle.png")
+        } 
+        else {
+            logo.classList.remove("nobrightness")
+            lineImg.setAttribute("src","images/line.png")
+            lineImg2.setAttribute("src","images/line.png")
+            circleImg.setAttribute("src","images/circle.png")
         }
     }
-    
-    let themeFlag = true; //white theme is enabled
+
+const seprator =$.getElementsByClassName("seprator");
+    function seperatorsHandler() {
+        for (let i = 0; i < seprator.length; i+=1) {
+
+            if (!themeFlag) {
+                seprator[i].setAttribute("src","images/blackseprator.png");
+            } else {
+                seprator[i].setAttribute("src","images/seprator.png");
+            }
+        }
+    }
+
+const iconify =$.getElementsByClassName("iconify");
+    function iconifyHandler() {
+        for (let i = 0; i < iconify.length; i+=1) {
+
+            if (!themeFlag) {
+                iconify[i].classList.replace("red","blue");
+            } else {
+                iconify[i].classList.replace("blue","red");
+            }
+        }
+    }
+
+const sections = $.getElementsByTagName("section"),
+      footer   = $.getElementsByTagName("footer"),
+      footerName=$.getElementById("name");
+    function sectionsHandler() {
+        for (let i = 0; i < sections.length; i++) {
+            themeFlag?
+            sections[i].classList.replace("light","dark"):
+            sections[i].classList.replace("dark","light")        
+        }
+        themeFlag?
+        footer[0].classList.replace("light","dark"):
+        footer[0].classList.replace("dark","light")
+
+        themeFlag?
+        footerName.classList.replace("blue","red"):
+        footerName.classList.replace("red","blue")
+
+    }
+
+/* const   blueBg = $.getElementsByClassName("blueBg");
+    function skillsHandler() {
+        for (let i = 0; i < blueBg.length; i++) {
+            console.log(i)
+            console.log(blueBg)
+            console.log(blueBg.length)
+            themeFlag?
+            blueBg[i].classList.replace("blueBg","redBg"):
+            blueBg[i].classList.replace("redBg","blueBg")      
+        }
+    } */
+
+
+const navArrow = $.getElementById("nav__arrow")
+    function navArrowHandler() {
+        themeFlag?
+        navArrow.classList.replace("dark","light"):
+        navArrow.classList.replace("light","dark");
+    }
+
+    // function whiteMode() {
+
+    //     let dark =$.getElementsByClassName("dark"),
+    //     light =$.getElementsByClassName("light");
+
+    //     // console.log(dark.length)
+    //     // console.log(light.length)
+
+    //     for (let i = 0; i < dark.length; i+=1) {
+    //          dark[i].classList.replace("dark","light");  
+    //         console.log("i="+i)
+    //     }
+
+    //     let indentdark =$.getElementsByClassName("indentdark");
+    //     for (let i = 0; i < indentdark.length; i+=1) {
+    //         indentdark[i].classList.replace("indentdark","indentWhite");
+    //         console.log("i="+i)  
+    //     }
+    // } 
+
+    //  function darkMode() {
+
+    //     let dark =$.getElementsByClassName("dark"),
+    //     light =$.getElementsByClassName("light");
+    //     // console.log(dark.length)
+    //     // console.log(dark)
+    //     // console.log(dark.length)
+
+    //     // console.log(light.length)
+    //     // console.log(light)
+
+    //     for (let i = 0; i < light.length; i+=1) {
+    //         light[i].classList.replace("light","dark");  
+    //         console.log("i="+i)
+    //     }
+
+    //     let indentWhite =$.getElementsByClassName("indentWhite");
+    //     for (let i = 0; i < indentWhite.length; i+=1) {
+    //         indentWhite[i].classList.replace("indentWhite","indentdark");
+    //         console.log("i="+i)  
+    //     }
+    // } 
 
     //theme btn
     $.getElementsByClassName("theme__btn__on")[0].addEventListener('click',function () {
-        //  console.log($.body.style.backgroundColor)
-        if (!themeFlag) {
-            whiteMode()
-            themeFlag = true
-        }
-        else {
-            darkMode()
+        if (themeFlag) {//Darkens The Theme
             themeFlag = false
+            $.title = "WHITE THEME"
+            $.body.style.setProperty("color", "#292D34", "important");
+            // darkMode()
         }
+        else {//Lightens The Theme
+            themeFlag = true
+            $.title = "DARK THEME"
+            $.body.style.setProperty("color", "#E7EBF0", "important");
+            // whiteMode()
+        }
+        // skillsHandler()
+        iconifyHandler()
+        seperatorsHandler()
+        picsHandler()
+        sectionsHandler()
+        navArrowHandler()
     })
-
-
