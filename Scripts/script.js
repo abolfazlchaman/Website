1a3598fa3fa6c55e91b97052ef3ca8a364ecd90b
Amir-n1k <satanfps.co@gmail.com>
2022-05-01 13:27:50 +0430

    Snippets for loader screen, offlineEvent, onlineEvent appended to js. Position & item corrections for theme btnSelector, Removed unused js

--- Scripts/script.js
+++ Scripts/script.js
@@ -39,103 +39,25 @@ function seperatorsHandler() {
   }
 }
 
-/* const iconify =$.getElementsByClassName("iconify");
-    function iconifyHandler() {
-        for (let i = 0; i < iconify.length; i+=1) {
-
-            if (!themeFlag) {
-                iconify[i].classList.replace("red","blue");
-            } else {
-                iconify[i].classList.replace("blue","red");
-            }
-        }
-    }
-
-const linkTags =$.getElementsByTagName("a");
-    function linkTagsHandler() {
-        for (let i = 0; i < linkTags.length; i+=1) {
-
-                linkTags[i].classList.toggle("txtwhite","#txtdark");
-
-        }
-    } */
-
-/* const sections = $.getElementsByTagName("section"),
-      footer   = $.getElementsByTagName("footer"),
-      footerName=$.getElementById("name");
-    function sectionsHandler() {
-        for (let i = 0; i < sections.length; i++) {
-            themeFlag?
-            sections[i].classList.replace("light","dark"):
-            sections[i].classList.replace("dark","light")        
-        }
-        themeFlag?
-        footer[0].classList.replace("light","dark"):
-        footer[0].classList.replace("dark","light")
-
-        themeFlag?
-        footerName.classList.replace("blue","red"):
-        footerName.classList.replace("red","blue")
-
-    } */
-
-/*     function skillsHandler() {
-        if (themeFlag) {
-        let   blueBg = $.getElementsByClassName("blueBg");
-            for (cntr=0;cntr<=4;cntr++) {
-                for (let i = 0; i < blueBg.length; i++) {
-                    blueBg[0].classList.replace("blueBg","redBg")
-                } 
-            }
-        } else {
-        let   redBg = $.getElementsByClassName("redBg");
-            for (cntr=0;cntr<=4;cntr++) {
-                for (let i = 0; i < redBg.length; i++) {
-                    redBg[0].classList.replace("redBg","blueBg"); 
-                } 
-            }
-        }
-
-    }  */
-
-/* const navArrow = $.getElementById("nav__arrow")
-    function navArrowHandler() {
-        themeFlag?
-        navArrow.classList.replace("dark","light"):
-        navArrow.classList.replace("light","dark");
-    } */
-
-/* const navBtn= navArrow.children;
-    function navBtnHandler() {
-    if (themeFlag) {
-                for (let i = 0; i < navBtn.length; i++) {
-                    navBtn[i].classList.replace("indentWhite","indentdark")
-                    navBtn[i].classList.replace("light","dark")
-                    // navBtn[i].classList.add("txtwhite")
-                    // navBtn[i].classList.replace("txtdark","txtwhite")
-
-                } 
-        } else {
-                for (let i = 0; i < navBtn.length; i++) {
-                    navBtn[i].classList.replace("indentdark","indentWhite")
-                    // navBtn[i].classList.replace("txtwhite","txtdark")
-                } 
-                }
-    } */
-
 $.getElementsByClassName("theme__btn__on")[0].addEventListener(
   "click",
   function () {
+
     if (themeFlag) {
       //Dark Theme
       themeFlag = false;
-
+      $.getElementsByClassName("theme__btn__on")[0].classList.replace("justifyend","justifystart")
+      $.getElementsByClassName("theme__btn__on")[0].classList.replace("btn__indent","btn__outdent")
       $.getElementById("stylesheet").setAttribute("href", "styles/white.css");
+      $.getElementById("icon1").setAttribute("data-icon","bi:moon-stars")
+      
     } else {
       //Light Theme
       themeFlag = true;
-
+      $.getElementsByClassName("theme__btn__on")[0].classList.replace("justifystart","justifyend")
+      $.getElementsByClassName("theme__btn__on")[0].classList.replace("btn__outdent","btn__indent")
       $.getElementById("stylesheet").setAttribute("href", "styles/dark.css");
+      $.getElementById("icon1").setAttribute("data-icon","heroicons-outline:sun")
     }
     seperatorsHandler();
     picsHandler();
@@ -196,18 +118,6 @@ for (let i = 0; i < childs.length; i++) {
     childs[i].classList.replace("btn__indent", "btn__outdent");
   });
 }
-// console.log(homeSection.offsetTop)
-// console.log(homeSection.offsetHeight)
-// console.log(aboutMeSection.offsetTop)
-
-// console.log(aboutMeSection.scrollHeight)
-// console.log(skillsSection.offsetTop)
-
-// console.log(skillsSection.scrollHeight)
-// console.log(services.scrollHeight)
-// console.log(worksSection.scrollHeight)
-// console.log(stepsSection.scrollHeight)
-// console.log(contactSection.scrollHeight)
 
 $.addEventListener("scroll", function name() {
   scrollHandler();
@@ -217,3 +127,13 @@ $.addEventListener("scroll", function name() {
 window.addEventListener('load', function() {
     console.log('All assets are loaded')
 })
+
+//offline modal here
+window.addEventListener('offline', (event) => {
+  console.log("The network connection has been lost.");
+});
+
+//online modal here
+window.addEventListener('online', (event) => {
+console.log("You are now connected to the network.");
+});
